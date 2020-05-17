41778ede4aca0fb2abe50ecb067f1dbbea94809f
yasin ! <yasinmhd110@gmail.com>
2020-05-18 01:13:58 +0430

    for test

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit
 from PyQt5 import uic
 import sys
 ##
-server = '0.0.0.0'#server address
+server = '1.0.0.0'#server address
 port = 8871
 
 uname = "hassan"
