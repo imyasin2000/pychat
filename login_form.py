2934ae89ae71bca75fc2556d2ff918219f7b8368
Mhfa_1380 <mhfa1380@gmail.com>
2020-05-18 12:41:10 +0430

    login rigester forget form added

--- login_form.py
+++ login_form.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit
 from PyQt5 import uic
 import sys
+import time
 
 
 
@@ -12,21 +13,26 @@ class UI_l(QMainWindow):
         self.textedit_2 = self.findChild(QTextEdit, "password_t")
         self.button_1 = self.findChild(QPushButton, "forget_b")
         self.button_2 = self.findChild(QPushButton, "login_b")
+        self.button_3 = self.findChild(QPushButton, "rigister_b")
         self.button_1.clicked.connect(self.clickedBtn_1)
         self.button_2.clicked.connect(self.clickedBtn_2)
+        self.button_3.clicked.connect(self.clickedBtn_3)
         self.show()
 
 
+
     def clickedBtn_1(self):#forget
-        self.myOtherWindow = UI_r()
+        self.myOtherWindow = UI_f()
         self.myOtherWindow.show()
         self.hide()
-
 
     def clickedBtn_2(self):#login
         pass
 
-
+    def clickedBtn_3(self):#rigester
+        self.myOtherWindow = UI_r()
+        self.myOtherWindow.show()
+        self.hide()
 
 class UI_r(QMainWindow):
     def __init__(self):
@@ -49,11 +55,36 @@ class UI_r(QMainWindow):
         pass
 
 
+class UI_f(QMainWindow):
+    def __init__(self):
+        super(UI_f, self).__init__()
+        uic.loadUi("Forget_F.ui", self)
+        self.textedit1 = self.findChild(QTextEdit, "textEdit")
+        self.button_1 = self.findChild(QPushButton, "pushButton")
+        self.button_1.clicked.connect(self.clickedBtn_1)
 
 
+        self.show()
 
 
+    def clickedBtn_1(self):#forget
+        # self.frame1.setVisible(True)
+        for i in range (2):
+            time.sleep(.2)
+            self.resize((self.frameGeometry().width()),self.frameGeometry().height())
 
-app = QApplication(sys.argv)
-window = UI_l()
-app.exec_()
+
+    def clickedBtn_2(self):#login
+        pass
+
+
+try:
+    f = open("lsc_u.txt")
+    app = QApplication(sys.argv)
+    window = UI_l()
+    app.exec_()
+except IOError:
+    print("File not accessible")
+finally:
+    f.close()
+
