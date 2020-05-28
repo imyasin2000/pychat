10a72af3ed5931e3268b728d07059c6a535ab197
Mhfa_1380 <mhfa1380@gmail.com>
2020-05-28 15:46:30 +0430

    chat box barname tarahi shodmfix few bugs

--- Version_2/reusable/Chat_Ui/new_login.py
+++ Version_2/reusable/Chat_Ui/new_login.py
@@ -6,50 +6,56 @@ from PyQt5 import uic
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QScrollArea, QVBoxLayout, QGroupBox, QLabel, QPushButton, QFormLayout
 import sys
+from PyQt5.QtCore    import *
+from PyQt5.QtGui     import *
+from PyQt5.QtWidgets import *
+
+# class Window(QWidget):
+#     def __init__(self):
+#         super().__init__()
 
 class Window(QMainWindow):
     
     def __init__(self):
-        super(Window, self).__init__()
-        uic.loadUi("123.ui", self)
-        
-        
-        self.groupBox = self.findChild(QGroupBox, "groupBox")
-        self.formLayout = self.findChild(QFormLayout, "formLayout_2")
-        
-        self.labelLis = []
-        self.comboList = []
-        # self.groupBox.setLayout(self.formLayout)
-        scroll = QScrollArea()
-        scroll.setWidget(self.groupBox)
-        scroll.setWidgetResizable(True)
-        scroll.setFixedHeight(400)
-        layout = QVBoxLayout(self)
-        layout.addWidget(scroll)
+        super().__init__()
+        uic.loadUi("Chat_box.ui", self)    
+
+        # self.labelLis = []
+        # self.labelLis_2 = []
+
         self.show()
 
-    def add(self):
-        self.labelLis.append(QLabel("Label"))
-        self.comboList.append(QPushButton("Click Me"))
-        self.formLayout.addRow(self.labelLis[-1], self.comboList[-1])
+    def add_me(self):
+        # self.labelLis.append(QLabel("Label1"))
+        # self.labelLis_2.append(QLabel("Label2"))
+        self.formLayout.addRow(QLabel("You : salam"))
+        # self.formLayout.addRow(QLabel("Khobi? : Ali",alignment=Qt.AlignRight))
+    
+    def add_other(self):
+        # self.labelLis.append(QLabel("Label1"))
+        # self.labelLis_2.append(QLabel("Label2"))
+        # self.formLayout.addRow(QLabel("You : salam"))
+        self.formLayout.addRow(QLabel("Khobi? : Ali",alignment=Qt.AlignRight))
     
     def dele(self,size):
-        for i in  range(size):
-            self.formLayout.removeRow(self.labelLis[-1])
-            del self.labelLis[-1]
-            del self.comboList[-1]
-
+        # for i in  range(size):
+        #     self.formLayout.removeRow(self.labelLis[-1])
+        #     del self.labelLis[-1]
+        #     del self.comboList[-1]
+        pass
 
 
 App = QApplication(sys.argv)
 
 
 window2 = Window()
-window2.add()
-window2.add()
-window2.add()
-window2.add()
-window2.dele(2)
 
-
+while (True):
+    x=(input("enter 1 : add me, enter 2 : add other \n"))
+    if x=='1':
+        window2.add_me()
+    elif x=='2':
+        window2.add_other()
+    else:
+        break
 sys.exit(App.exec())
