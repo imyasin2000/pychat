829166d0776192c25182052cab29e6c89347bae8
Mhfa_1380 <mhfa1380@gmail.com>
2020-05-29 12:33:48 +0430

    send sms invitelink add shod va chand bug fix shod

--- Version_2/reusable/sms_sender.py
+++ Version_2/reusable/sms_sender.py
@@ -1,4 +1,9 @@
 import ghasedak
 
-sms = ghasedak.Ghasedak("a953ba30854130502559586ca6b93e0f81de604661d4bd0ec858e162330f8a76")
-sms.send({ 'message':'Welcome to PyChat!\n\n' + 'Youre Code Is : ' + '653265',  'receptor' : '09113187038',  'linenumber': '10008566' })
+def sms_sender(from_c,send_to):
+    sms = ghasedak.Ghasedak("a953ba30854130502559586ca6b93e0f81de604661d4bd0ec858e162330f8a76")
+    sms.send({ 'message':str(from_c) +' invited you to chat on PyChat.\n\nYou can download PyChat with the link below : \n\n' + "http://8upload.ir/uploads/f74418985.zip",  'receptor' : str(send_to),  'linenumber': '10008566' })
+
+
+
+sms_sender("Yasin Khamar","09113187038")
