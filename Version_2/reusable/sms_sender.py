e1b5a0a66852a56f3dbeed35e9da75aaaaad5dab
Mhfa_1380 <mhfa1380@gmail.com>
2020-05-29 11:56:12 +0430

    ersal sms be reusable ezafe shod

---
+++ Version_2/reusable/sms_sender.py
@@ -0,0 +1,4 @@
+import ghasedak
+
+sms = ghasedak.Ghasedak("a953ba30854130502559586ca6b93e0f81de604661d4bd0ec858e162330f8a76")
+sms.send({ 'message':'Welcome to PyChat!\n\n' + 'Youre Code Is : ' + '653265',  'receptor' : '09113187038',  'linenumber': '10008566' })
