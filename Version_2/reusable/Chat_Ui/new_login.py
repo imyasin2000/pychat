6f8e22e7662dae4982252bc2c232cc09f8134f08
Mhfa_1380 <mhfa1380@gmail.com>
2020-05-31 19:13:56 +0430

    ui maste(chatbox) add shod

--- Version_2/reusable/Chat_Ui/new_login.py
+++ Version_2/reusable/Chat_Ui/new_login.py
@@ -9,53 +9,81 @@ import sys
 from PyQt5.QtCore    import *
 from PyQt5.QtGui     import *
 from PyQt5.QtWidgets import *
+import os
 
-# class Window(QWidget):
-#     def __init__(self):
-#         super().__init__()
 
 class Window(QMainWindow):
     
     def __init__(self):
         super().__init__()
-        uic.loadUi("Chat_box.ui", self)    
-
-        # self.labelLis = []
-        # self.labelLis_2 = []
+        uic.loadUi("Chat_box.ui", self) 
+        self.textedit_messegebox = self.findChild(QTextEdit, "messegebox_t")
+        # self.label = self.findChild(QLabel, "label")
+        self.button_user = self.findChild(QPushButton, "user_b")
+        self.button_send = self.findChild(QPushButton, "send_b")
+        self.button_other = self.findChild(QPushButton, "other_b")
+        self.button_clear = self.findChild(QPushButton, "clear_b")
+        self.button_attach = self.findChild(QPushButton, "attach_b")
+        self.button_record = self.findChild(QPushButton, "record_b")
+        self.button_send.clicked.connect(self.clickedBtn_send)
+        self.button_other.clicked.connect(self.clickedBtn_other) 
+        self.button_clear.clicked.connect(self.clickedBtn_clear)   
+        self.button_user.clicked.connect(self.clickedBtn_user)   
+        self.textedit_messegebox.textChanged.connect(self.textChanged_messege_event)
+        
+       
+        
+        self.button_record.setHidden(False)
+        self.button_send.setHidden(True)
+      
+       
 
         self.show()
 
-    def add_me(self):
-        # self.labelLis.append(QLabel("Label1"))
-        # self.labelLis_2.append(QLabel("Label2"))
-        self.formLayout.addRow(QLabel("You : salam"))
-        # self.formLayout.addRow(QLabel("Khobi? : Ali",alignment=Qt.AlignRight))
-    
-    def add_other(self):
-        # self.labelLis.append(QLabel("Label1"))
-        # self.labelLis_2.append(QLabel("Label2"))
-        # self.formLayout.addRow(QLabel("You : salam"))
-        self.formLayout.addRow(QLabel("Khobi? : Ali",alignment=Qt.AlignRight))
-    
-    def dele(self,size):
-        # for i in  range(size):
-        #     self.formLayout.removeRow(self.labelLis[-1])
-        #     del self.labelLis[-1]
-        #     del self.comboList[-1]
-        pass
+   
+        
+
+    def clickedBtn_send(self):
+        if self.textedit_messegebox.toPlainText().strip() :
+            self.user_image = QLabel()
+            self.user_image.setPixmap(QPixmap(os.path.abspath(os.getcwd()+'/me.png')).scaledToWidth(40))
+            self.messege_user = QLabel(self.textedit_messegebox.toPlainText())
+            self.messege_user.setStyleSheet("background-color: white;border: 2px solid teal;") 
+            self.textedit_messegebox.clear()
+            self.formLayout.addRow(self.user_image,self.messege_user)
+
+    def clickedBtn_other(self):
+        self.user_image = QLabel()
+        self.user_image.setPixmap(QPixmap(os.path.abspath(os.getcwd()+'/other.png')).scaledToWidth(40))
+        self.messege_user = QLabel(" salam mmd")
+        self.messege_user.setStyleSheet("background-color: lightgreen;border: 1px solid teal;") 
+        self.formLayout.addRow(self.user_image,self.messege_user)
+
+    def clickedBtn_user(self):
+        self.user_image = QLabel()
+        self.user_image.setPixmap(QPixmap(os.path.abspath(os.getcwd()+'/other.png')).scaledToWidth(40))
+        self.name_user = QLabel("\nMmd Hossein\n")
+        self.name_user.setStyleSheet("background-color: white;border: 1px solid teal;") 
+        self.formLayout_2.addRow(self.user_image,self.name_user)
+        
+
+    def textChanged_messege_event(self):
+        
+        if self.textedit_messegebox.toPlainText().strip() :
+            self.button_record.setHidden(True)
+            self.button_send.setHidden(False)
+        else:
+            self.button_record.setHidden(False)
+            self.button_send.setHidden(True)
+
+
+        
+    def clickedBtn_clear(self):
+        for i in reversed(range(self.formLayout.count())): 
+            self.formLayout.itemAt(i).widget().deleteLater()
+        
 
 
 App = QApplication(sys.argv)
-
-
 window2 = Window()
-
-while (True):
-    x=(input("enter 1 : add me, enter 2 : add other \n"))
-    if x=='1':
-        window2.add_me()
-    elif x=='2':
-        window2.add_other()
-    else:
-        break
 sys.exit(App.exec())
