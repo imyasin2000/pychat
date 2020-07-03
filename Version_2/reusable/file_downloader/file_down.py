d2c0e95f92727a09356b79f436cbd5d55271bb05
Mhfa_1380 <mhfa1380@gmail.com>
2020-07-03 13:01:55 +0430

    signup with yahoo be reusable add shod

--- Version_2/reusable/file_downloader/file_down.py
+++ Version_2/reusable/file_downloader/file_down.py
@@ -7,5 +7,5 @@ print('Beginning file download with urllib2...')
 url = 'http://8upload.ir/uploads/f0371645.mp4'
 print(os.getcwd()+"/cat.mp4")
 # os.popen("cat.mp4")
-# urllib.request.urlretrieve(url, 'cat.mp4')
+urllib.request.urlretrieve(url, 'cat.mp4')
 
