31421c9a66906cbdf3015f0189f1624ceeeaf112
Mhfa_1380 <mhfa1380@gmail.com>
2020-07-04 09:03:13 +0430

    text to spech takmil shod + ahang zang ezafe shod + va ahang zang va incoming call from ba ham digar mix va play mishavand.

--- Version_2/reusable/text-to-speach.py
+++ Version_2/reusable/text-to-speach.py
@@ -1,6 +1,16 @@
 import gtts
-from playsound import playsound
+from playsound import playsound #++sudo apt-get install ffmpeg
+import os
+from pydub import AudioSegment
+from pydub.playback import play
+
 
 tts = gtts.gTTS('incoming call from'+ 'mohammad hossein fadavi')
-tts.save("hello.mp3")
-playsound("hello.mp3")
+tts.save(os.getcwd()+'/incoming_call.mp3')
+audio1 = AudioSegment.from_file(os.getcwd()+"/apple-orginal.mp3")
+audio2 = AudioSegment.from_file(os.getcwd()+'/incoming_call.mp3')
+mixed = audio1.overlay(audio2)  
+for i in range(0,2):#78 sec
+    play(mixed) 
+    play(audio1) 
+os.remove(os.getcwd()+'/incoming_call.mp3') 
