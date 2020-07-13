a8e49fd8c69423a391016c8c085ac2643e2ee328
Mhfa_1380 <mhfa1380@gmail.com>
2020-07-13 08:42:20 +0430

    bug chat resizble fix + file receve ui ezafe shod + some color changes

--- Version_2/reusable/other/circular pic.py
+++ Version_2/reusable/other/circular pic.py
@@ -2,14 +2,19 @@
 from PIL import Image, ImageOps, ImageDraw
 import os
 
+#circle pic
 size = (500, 500)
 mask = Image.new('L', size, 0)
 draw = ImageDraw.Draw(mask)
 draw.ellipse((0, 0) + size, fill=255)
-
-im = Image.open(os.path.abspath(os.getcwd()+'/pychat.png'))
-
+from PIL import ImageFilter
+im = Image.open(os.path.abspath(os.getcwd()+'/output copy.png'))
 output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
 output.putalpha(mask)
-
 output.save('output.png')
+
+#blur pic
+from PIL import ImageFilter
+
+blurred_image = im.filter(ImageFilter.BLUR)
+blurred_image.save('output3.png')
